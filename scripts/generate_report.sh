1023a53b1516ad5c84c60874c2d9b24bcc2589b0
ilevcovitz <ivan.levcovitz@gmail.com>
2016-03-21 11:57:45 -0400

    removed irrelevant files

--- scripts/generate_report.sh
+++
@@ -1,8 +0,0 @@
-@echo off
-cd "C:\Users\Av\Documents\GitHub\tutoring_reports"
-git pull
-runipy "C:\Users\Av\Documents\GitHub\tutoring_reports\report.ipynb" "C:\Users\Av\Documents\GitHub\tutoring_reports\report.ipynb"
-git add .
-git commit -am "new report"
-git push
-pause
--- scripts/run_sign_in.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-C:\Users\Av\Anaconda3\python.exe "C:\tutoring\sign_in\sign_in.py" runserver %*
-pause
